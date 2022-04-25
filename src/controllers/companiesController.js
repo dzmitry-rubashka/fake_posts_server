aab0db621e4a79dc39d0c67d74f65d01996eb9de
Dzmitry Rubashka <dm.rubashko@gmail.com>
2022-04-26 00:43:57 +0300

    Changed some controllers

--- src/controllers/companiesController.js
+++ src/controllers/companiesController.js
@@ -6,11 +6,12 @@ const { Pool } = pkg;
 const poolCreator = new Pool(pool);
 
 class CompaniesController {
+  //++
   async getAllCompanies(req, res) {
     const companies = await poolCreator.query(`SELECT * FROM company`);
     res.json(companies.rows);
   }
-
+//++
   async getOneCompany(req, res) {
     const id = req.params.id;
     const company = await poolCreator.query(
@@ -19,7 +20,7 @@ class CompaniesController {
     );
     res.json(company.rows[0]);
   }
-
+//++
   async deleteCompany(req, res) {
     const id = req.params.id;
     const company = await poolCreator.query(
@@ -28,7 +29,7 @@ class CompaniesController {
     );
     res.json(company.rows[0]);
   }
-
+//++
   async createCompany(req, res) {
     const { name, catchphrase, bs } = req.body;
     const newCompany = await poolCreator.query(
@@ -37,7 +38,7 @@ class CompaniesController {
     );
     res.json(newCompany.rows[0]);
   }
-
+//++
   async updateCompany(req, res) {
     const { name, catchphrase, bs, id } = req.body;
     const updatedCompany = await poolCreator.query(
