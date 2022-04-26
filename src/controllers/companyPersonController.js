d6ab635fe26d7c86d0765b7c5458ddef04ce11ce
Dzmitry Rubashka <dm.rubashko@gmail.com>
2022-04-26 18:51:01 +0300

    Added company_person relation deleting

--- src/controllers/companyPersonController.js
+++ src/controllers/companyPersonController.js
@@ -12,7 +12,15 @@ class CompanyPersonController {
       `INSERT INTO company_person (company_id, user_id) values ($1, $2) RETURNING *`,
       [company_id, user_id]
     );
-    res.json(newRelations.rows);
+    res.json(newRelations.rows[0]);
+  }
+  async deleteCompanyPersonRelation(req, res) {
+    const { user_id, company_id } = req.body;
+    const deletedRElation = await poolCreator.query(
+      `DELETE FROM company_person where user_id = $1 and company_id = $2`,
+      [user_id, company_id]
+    );
+    res.json(deletedRElation.rows[0]);
   }
 }
 
--- src/routes/companyPersonRoutes.js
+++ src/routes/companyPersonRoutes.js
@@ -5,5 +5,7 @@ import CompanyPersonController from "../controllers/companyPersonController.js";
 const router = new Router();
 
 router.post("/company_person", CompanyPersonController.createCompanyPersonRelation);
+router.delete("/company_person", CompanyPersonController.deleteCompanyPersonRelation);
+
 
 export default router;
